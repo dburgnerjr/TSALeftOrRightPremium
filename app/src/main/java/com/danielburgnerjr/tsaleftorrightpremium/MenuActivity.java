f3529f4aecbf4279057979c49bb39e6058b030dd
Daniel Burgner <dburgnerjr@yahoo.com>
2016-04-07 15:08:54 -0400

    Menu Activity complete.
    Completed Menu Activiity

--- app/src/main/java/com/danielburgnerjr/tsaleftorrightpremium/MenuActivity.java
+++ app/src/main/java/com/danielburgnerjr/tsaleftorrightpremium/MenuActivity.java
@@ -1,37 +1,50 @@
 package com.danielburgnerjr.tsaleftorrightpremium;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.view.Menu;
-import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class MenuActivity extends AppCompatActivity {
 
+    String strPackName;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        strPackName = getApplicationContext().getPackageName();
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_menu);
+        final Button btnAbout = (Button) findViewById(R.id.btnAbout);
+        btnAbout.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View view) {
+                Toast.makeText(getApplicationContext(), "Coming soon", Toast.LENGTH_SHORT).show();
+                //Intent intA = new Intent(MenuActivity.this, AboutActivity.class);
+                //startActivity(intA);
+            }
+        });
+
+        final Button btnStart = (Button) findViewById(R.id.btnStart);
+        btnStart.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View view) {
+                Toast.makeText(getApplicationContext(), "Coming soon", Toast.LENGTH_SHORT).show();
+                //Intent intA = new Intent(MenuActivity.this, RandomActivity.class);
+                //startActivity(intA);
+            }
+        });
+
+        final Button btnShare = (Button) findViewById(R.id.btnShare);
+        btnShare.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View view) {
+                Intent intI = new Intent(Intent.ACTION_SEND);
+                intI.setType("text/plain");
+                intI.putExtra(Intent.EXTRA_SUBJECT, "TSA Left or Right Premium");
+                String sAux = "\nLet me recommend you this application\n\n";
+                sAux = sAux + "https://play.google.com/store/apps/details?id=" + strPackName + "\n\n";
+                intI.putExtra(Intent.EXTRA_TEXT, sAux);
+                startActivity(Intent.createChooser(intI, "choose one"));
+            }
+        });
     }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_menu, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
-    }
 }
