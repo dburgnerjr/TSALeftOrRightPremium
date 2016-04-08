f7267c132f116a60207971bc51f037c0fa2c13b4
Daniel Burgner <dburgnerjr@yahoo.com>
2016-04-08 16:20:15 -0400

    Dev Complete.
    Completed development work for this app.

--- app/src/main/java/com/danielburgnerjr/tsaleftorrightpremium/RandomActivity.java
+++ app/src/main/java/com/danielburgnerjr/tsaleftorrightpremium/RandomActivity.java
@@ -3,56 +3,70 @@ package com.danielburgnerjr.tsaleftorrightpremium;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.view.animation.AlphaAnimation;
+import android.view.animation.Animation;
 import android.widget.Button;
 import android.widget.ImageView;
 
-import java.util.*;
+import java.util.Random;
 
 public class RandomActivity extends AppCompatActivity {
 
     private ImageView imvLeftArrow;
     private ImageView imvRightArrow;
     private Button btnTryAgain;
+    private Animation aniTransition;
+    private static int SPLASH_TIME_OUT = 5000;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_random);
 
-        imvLeftArrow   = (ImageView)findViewById(R.id.imgLeft);
-        imvRightArrow   = (ImageView)findViewById(R.id.imgRight);
-        btnTryAgain   = (Button)findViewById(R.id.btnTryAgain);
+        imvLeftArrow = (ImageView) findViewById(R.id.imgLeft);
+        imvRightArrow = (ImageView) findViewById(R.id.imgRight);
+        btnTryAgain = (Button) findViewById(R.id.btnTryAgain);
 
-        imvLeftArrow.setVisibility(View.GONE);
-        imvRightArrow.setVisibility(View.GONE);
-        btnTryAgain.setVisibility(View.GONE);
+        aniTransition = new AlphaAnimation(1, 0);
+        aniTransition.setDuration(SPLASH_TIME_OUT);
+        aniTransition.setStartOffset(SPLASH_TIME_OUT);
+        imvLeftArrow.setAnimation(aniTransition);
+        imvRightArrow.setAnimation(aniTransition);
 
-        randomize();
-    }
+        aniTransition.setAnimationListener(new Animation.AnimationListener() {
+            @Override
+            public void onAnimationStart(Animation animation) {
 
-    protected void randomize() {
-        Random rndR = new Random();
-        int nChoice = rndR.nextInt(2);
+            }
 
-        // if nChoice = 0 left arrow, else right arrow
-        if (nChoice == 0) {
-            imvLeftArrow.setVisibility(View.VISIBLE);
-            imvRightArrow.setVisibility(View.INVISIBLE);
-            btnTryAgain.setVisibility(View.INVISIBLE);
-        } else {
-            imvLeftArrow.setVisibility(View.INVISIBLE);
-            imvRightArrow.setVisibility(View.VISIBLE);
-            btnTryAgain.setVisibility(View.INVISIBLE);
-        }
-        for (int nI = 0; nI < 5000; nI++) {
+            @Override
+            public void onAnimationEnd(Animation animation) {
+                imvLeftArrow.setVisibility(View.GONE);
+                imvRightArrow.setVisibility(View.GONE);
+                btnTryAgain.setVisibility(View.VISIBLE);
+            }
 
-        }
-        if (nChoice == 0) {
-            imvLeftArrow.setVisibility(View.INVISIBLE);
-        } else {
-            imvRightArrow.setVisibility(View.INVISIBLE);
-        }
-        btnTryAgain.setVisibility(View.VISIBLE);
-    }
+            @Override
+            public void onAnimationRepeat(Animation animation) {
+
+            }
+        });
+
+
+        btnTryAgain.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View view) {
+                Random rndR = new Random();
+                int nChoice = rndR.nextInt(2);
+                btnTryAgain.setVisibility(View.GONE);
+                if (nChoice == 0) {
+                    imvLeftArrow.setVisibility(View.VISIBLE);
+                    imvLeftArrow.startAnimation(aniTransition);
+                } else {
+                    imvRightArrow.setVisibility(View.VISIBLE);
+                    imvRightArrow.startAnimation(aniTransition);
+                }
+            }
+        });
+     }
 }
 
